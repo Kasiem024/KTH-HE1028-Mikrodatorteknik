ea0ed4150e67ad3959379a72f8c430495b2cc707
Kasiem Saeed <kasiemsaeed@gmail.com>
2025-02-24 21:54:00 +0100

    finished with lab 5_2

--- LAB 5_2/main.c
+++ LAB 5_2/main.c
@@ -6,102 +6,161 @@
 #define EI 1
 #define DI 0
 
-void rtcInit(void){
-   // enable power managemenet unit - perhaps enabled by default
-   rcu_periph_clock_enable(RCU_PMU);
-   // enable write access to the registers in the backup domain
-   pmu_backup_write_enable();
-   // enable backup domain
-   rcu_periph_clock_enable(RCU_BKPI);
-   // reset backup domain registers
-   bkp_deinit();
-   // set the results of a previous calibration procedure
-   // bkp_rtc_calibration_value_set(x);
+int main(void)
+{
+  int key, currentReceiveIndex = 0, currentSendIndex = 0, sendDataNow = 0, amountCharSent = 0, currentLine = 0;
+  char transmitData[7];
+  int lookUpTbl[16] = {1, 4, 7, 14, 2, 5, 8, 0, 3, 6, 9, 15, 10, 11, 12, 13};
 
-   // setup RTC
-   // enable external low speed XO
-   //rcu_osci_on(RCU_LXTAL);
-   if (rcu_osci_stab_wait(RCU_HXTAL)) {
-     // use external low speed oscillaotr, i.e. 32.768 kHz
-     rcu_rtc_clock_config(RCU_RTCSRC_HXTAL_DIV_128);
-     rcu_periph_clock_enable(RCU_RTC);
-     // wait until shadow registers are synced from the backup domain
-     // over the APB bus
-     rtc_register_sync_wait();
-     // wait until shadow register changes are synced over APB
-     // to the backup doamin
-     rtc_lwoff_wait();
-     // prescale to 1 second
-     rtc_prescaler_set(62500 - 1);
-     rtc_lwoff_wait();
-     rtc_flag_clear(RTC_INT_FLAG_SECOND);
-     //rtc_interrupt_enable(RTC_INT_SECOND);
-     rtc_lwoff_wait();
-   }
-}
+  t5omsi();                // Initialize timer5 1kHz
+  colinit();               // Initialize column toolbox
+  l88init();               // Initialize 8*8 led toolbox
+  keyinit();               // Initialize keyboard toolbox
+  ADC3powerUpInit(1);      // Initialize ADC0, Ch3
+  Lcd_SetType(LCD_NORMAL); // or use LCD_INVERTED!
+  Lcd_Init();
+  LCD_Clear(BLACK);
+  u0init(EI);                      // Initialize USART0 toolbox
+  eclic_global_interrupt_enable(); // !!! INTERRUPT ENABLED !!!
 
-int main(void){
-    int ms=0, s=0, key, pKey=-1, c=0, idle=0, rtc, hh, mm, ss;
-    int lookUpTbl[16]={1,4,7,14,2,5,8,0,3,6,9,15,10,11,12,13};
-    int dac=0, speed=-100;
-    int adcr, tmpr;
-    char digits[10][10]={"Zero ","One  ","Two  ","Three","Four ","Five ","Six  ","Seven","Eight","Nine "};
-    //char time[7]={0};
+  while (1)
+  {
+    LCD_WR_Queue(); // Manage LCD com queue!
+    u0_TX_Queue();  // Manage U(S)ART TX Queue!
 
-    t5omsi();                               // Initialize timer5 1kHz
-    colinit();                              // Initialize column toolbox
-    l88init();                              // Initialize 8*8 led toolbox
-    keyinit();                              // Initialize keyboard toolbox
-    ADC3powerUpInit(1);                     // Initialize ADC0, Ch3
-    Lcd_SetType(LCD_NORMAL);                // or use LCD_INVERTED!
-    Lcd_Init();
-    LCD_Clear(RED);
-    LCD_ShowStr(10, 10, "Lab #5", WHITE, TRANSPARENT);
-    //rtcInit();                              // Initialize RTC
-    //rtc_counter_set(3600+60+1);
-    u0init(EI);                             // Initialize USART0 toolbox
+    if (t5expq())
+    {
+      l88row(colset()); // ...8*8LED and Keyboard
 
-    eclic_global_interrupt_enable();        // !!! INTERRUPT ENABLED !!!
+      if (sendDataNow)
+      {
+        char currentChar = (int)transmitData[currentSendIndex];
+        usart_data_transmit(USART0, currentChar);
+        currentSendIndex++;
 
-    while (1) {
-        idle++;                             // Manage Async events
-        LCD_WR_Queue();                     // Manage LCD com queue!
-        //u0_TX_Queue();                      // Manage U(S)ART TX Queue!
-        if (adc_flag_get(ADC0,ADC_FLAG_EOC)==SET) { // ...ADC done?
-          if (adc_flag_get(ADC0,ADC_FLAG_EOIC)==SET) { //...ch3 or ch16?
-            tmpr = adc_inserted_data_read(ADC0, ADC_INSERTED_CHANNEL_0);
-            //l88mem(6,((0x680-tmpr)/5)+25);
-            //usart_data_transmit(USART0, ? ); // USRAT0 TX!
-            adc_flag_clear(ADC0, ADC_FLAG_EOC);
-            adc_flag_clear(ADC0, ADC_FLAG_EOIC);
-          } else {
-            adcr = adc_regular_data_read(ADC0); // ......get data
-            l88mem(4,adcr>>8);                  // ......move data
-            l88mem(5,adcr);                     // ......(view each ms)
-            adc_flag_clear(ADC0, ADC_FLAG_EOC); // ......clear IF
+        if (currentSendIndex == amountCharSent) // If we have sent the whole word
+        {
+          sendDataNow = 0;
+          currentSendIndex = 0;
+          amountCharSent = 0;
+        }
+      }
+
+      if ((key = keyscan()) >= 0)
+      {
+        if (key == 12) // A
+        {
+          adc_software_trigger_enable(ADC0, ADC_REGULAR_CHANNEL); // Trigger another ADC conversion!
+
+          if (adc_flag_get(ADC0, ADC_FLAG_EOC) == SET)
+          { // ...ADC done?
+            if (adc_flag_get(ADC0, ADC_FLAG_EOIC) == SET)
+            { //...ch3 or ch16?
+              int tmpr = adc_inserted_data_read(ADC0, ADC_INSERTED_CHANNEL_0);
+
+              adc_flag_clear(ADC0, ADC_FLAG_EOC);
+              adc_flag_clear(ADC0, ADC_FLAG_EOIC);
+
+              tmpr = ((0x680 - tmpr) / 5) + 25;
+
+              int sign = '+'; // +
+              if (tmpr < 0)
+              {
+                sign = '-';   // -
+                tmpr = -tmpr; // Make tmpr positive for consistent handling
+              }
+
+              int hundredsDigit = '0';
+              if (tmpr >= 100)
+              {
+                hundredsDigit = '1';
+                tmpr = tmpr % 100; // Extract the remaining two digits.
+              }
+
+              char tensDigit, unitsDigit;
+
+              tensDigit = (tmpr / 10) + '0';
+              unitsDigit = (tmpr % 10) + '0';
+
+              char newDataTransmit[7];
+
+              newDataTransmit[0] = 'I';
+              newDataTransmit[1] = 'D';
+              newDataTransmit[2] = sign;
+              newDataTransmit[3] = hundredsDigit;
+              newDataTransmit[4] = tensDigit;
+              newDataTransmit[5] = unitsDigit;
+              newDataTransmit[6] = 10;
+
+              amountCharSent = sizeof(newDataTransmit);
+
+              for (int i = 0; i < amountCharSent; i++)
+              {
+                transmitData[i] = newDataTransmit[i];
+              }
+
+              sendDataNow = 1;
+            }
           }
         }
-        if (usart_flag_get(USART0,USART_FLAG_RBNE)){ // USART0 RX?
-          //l88mem(6,usart_data_receive(USART0)); // Yes: Retrive & display!
-          LCD_ShowChar(10,50,usart_data_receive(USART0), OPAQUE, WHITE);
-        }
+        else if (key == 13) // B
+        {
+          char newDataTransmit[7] = "ID-000\n";
+          amountCharSent = sizeof(newDataTransmit);
 
-        if (t5expq()) {                     // Manage periodic tasks
-            l88row(colset());               // ...8*8LED and Keyboard
-            ms++;                           // ...One second heart beat
-            if (ms==1000){
-              ms=0;
-              l88mem(0,s++);
-              putch('A'+s%10);
-            }
-            if ((key=keyscan())>=0) {       // ...Any key pressed?
-              if (pKey==key) c++; else {c=0; pKey=key;}
-              l88mem(1,lookUpTbl[key]+(c<<4));
-            }
-            l88mem(2,idle>>8);              // ...Performance monitor
-            l88mem(3,idle); idle=0;
-            adc_software_trigger_enable(ADC0, //Trigger another ADC conversion!
-                                        ADC_REGULAR_CHANNEL);
+          for (int i = 0; i < amountCharSent; i++)
+          {
+            transmitData[i] = newDataTransmit[i];
+          }
+
+          sendDataNow = 1;
         }
+        else if (key == 14) // C
+        {
+          char newDataTransmit[7] = "ID*000\n";
+          amountCharSent = sizeof(newDataTransmit);
+
+          for (int i = 0; i < amountCharSent; i++)
+          {
+            transmitData[i] = newDataTransmit[i];
+          }
+
+          sendDataNow = 1;
+        }
+      }
+
+      if (usart_flag_get(USART0, USART_FLAG_RBNE))
+      {
+        int receivedData = usart_data_receive(USART0);
+        int receivedDataArray[7];
+
+        receivedDataArray[currentReceiveIndex] = receivedData;
+
+        if (receivedData == 10)
+        {
+          if (receivedDataArray[0] == 'I' &&
+              receivedDataArray[1] == 'D' &&
+              (receivedDataArray[2] == '+' || receivedDataArray[2] == '-') &&
+              isdigit(receivedDataArray[3]) && // Check if it's a digit '0' to '9'
+              isdigit(receivedDataArray[4]) &&
+              isdigit(receivedDataArray[5]))
+          {
+            LCD_ShowStr(currentReceiveIndex * 10, currentLine * 15, " OK!", WHITE, TRANSPARENT);
+          }
+          else
+          {
+            LCD_ShowStr(currentReceiveIndex * 10, currentLine * 15, " ERR", WHITE, TRANSPARENT);
+          }
+
+          currentReceiveIndex = 0;
+          currentLine++;
+        }
+        else
+        {
+          LCD_ShowChar(currentReceiveIndex * 10, currentLine * 15, receivedData, TRANSPARENT, WHITE);
+          currentReceiveIndex++;
+        }
+      }
     }
+  }
 }
